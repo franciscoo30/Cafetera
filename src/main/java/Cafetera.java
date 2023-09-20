4431ad3d2c3485d6c9caaa1026f79de664863d71
diagnostico <f.barrozo02@ufromail.cl>
2023-09-20 10:28:53 -0300

    Primer commit

---
+++ src/main/java/Cafetera.java
@@ -0,0 +1,25 @@
+public class Cafetera {
+    double capacidad;
+    double agua;
+    double cafe;
+    String marca;
+    String color;
+    public void prepararCafe(){
+
+    }
+    public void agregarAgua(){
+
+    }
+    public void agregarCafe(){
+
+    }
+    public void encender(){
+
+    }
+    public void apagar(){
+
+    }
+    public String obtenerMarca(){
+
+    return marca;}
+}
